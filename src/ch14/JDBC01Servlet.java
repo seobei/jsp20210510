c9d6aeaa0480e4ee79def0b5059bc65c3066eea5
seobei <sys1774@nate.com>
2021-05-28 14:55:59 +0900

    jdbc

---
+++ src/ch14/JDBC01Servlet.java
@@ -0,0 +1,110 @@
+package ch14;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JDBC01Servlet
+ */
+@WebServlet("/JDBC01Servlet")
+public class JDBC01Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public JDBC01Servlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		executeJDBC();
+
+		response.getWriter().print("<h1>jdbc01</h1>");
+
+	}
+
+	private void executeJDBC() {
+		
+		String sql = "SELECT CustomerName FROM Customers WHERE CustomerID = 1";
+		
+		String url = "jdbc:mysql://52.78.42.226/test";
+		String user = "root";
+		String password = "wnddkdwjdqhcjfl1";
+		
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+		
+			Class.forName("com.mysql.cj.jdbc.Driver");	
+			
+			con = DriverManager.getConnection(url, user, password);
+		
+			stmt = con.createStatement();
+		
+			rs = stmt.executeQuery(sql);
+		
+			if (rs.next()) {
+				String name = rs.getString(1);
+				System.out.println(name);			
+		}
+		
+		
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null ) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(stmt != null ) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con != null ) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
