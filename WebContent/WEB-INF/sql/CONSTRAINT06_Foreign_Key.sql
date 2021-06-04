87ffe35afaf2b7c35fe79921429c9f44bf334d1e
seobei <sys1774@nate.com>
2021-06-04 16:16:45 +0900

    sql

---
+++ WebContent/WEB-INF/sql/CONSTRAINT06_Foreign_Key.sql
@@ -0,0 +1,49 @@
+USE test;
+
+CREATE TABLE MyTable17
+(
+	id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+DESC MyTable17;
+
+DROP TABLE MyTable18;
+CREATE TABLE MyTable18
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    t17Id VARCHAR(255) NOT NULL,
+    FOREIGN KEY(t17Id) REFERENCES MyTable17(id)
+);
+
+INSERT INTO MyTable17
+(id, name)
+VALUES
+('donald', 'trump');
+
+INSERT INTO MyTable18
+(title, t17Id)
+VALUES
+('usa', 'donald');
+
+SELECT * FROM MyTable18;
+
+INSERT INTO MyTable18
+(title, t17Id)
+VALUES
+('korea', 'kim'); -- x
+
+DELETE FROM MyTable17
+WHERE id = 'donald';
+
+DELETE FROM MyTable18
+WHERE t17Id = 'donald';
+
+
+
+
+
+
+
+
---
+++ WebContent/WEB-INF/sql/CREATE_TABLE04_Board_CRUD.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Board
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL,
+    body VARCHAR(2047) NOT NULL,
+    memberId VARCHAR(255) NOT NULL,
+    inserted TIMESTAMP DEFAULT NOW()
+);
+DESC Board;
+
+SELECT * FROM Board;
+SELECT * FROM Member;
+
+use test2;
+
+ALTER TABLE Board
+ADD FOREIGN KEY (MemberId)
+REFERENCES Member(id);
+
+
+
+
